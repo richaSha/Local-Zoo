50dbf9f54e6376a405ffb040d9ee3d340c92f47f
richa.shaurbh <richa.shaurabh@gmail.com>
2017-10-27 14:58:40 -0700

    add view animal page

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,136 +4,134 @@ import {Component} from "@angular/core";
   selector: 'app-root',
   template: `
     <div class="container">
-      <div *ngIf="!type" >
-        <button class="btn btn-info" (click)="showMenu('Employee')">Enter As a Employee</button>
-        <button class="btn btn-info" (click)="showMenu('Patron')">Enter As a Patron</button>
+      <div *ngIf="!pagination" >
+        <button class="btn btn-info" (click)="goToPage('new')">Add New Animal</button>
+        <button class="btn btn-info" (click)="goToPage('all')">View All Animal</button>
+        <button class="btn btn-info" (click)="goToPage('young')">View Young Animal</button>
+        <button class="btn btn-info" (click)="goToPage('mature')">View Mature Animal</button>
       </div>
-      <div *ngIf="type">
-        <h1>{{title}}</h1>
-        <div class="menu">
-          <div class='menuTitle'>
-            <span *ngIf="editMode">
-              Select
-            </span>
-            <span class='type'>Type</span>
-            <span class='brand'>Brand</span>
-            <span class='alcohal'> Alcohal</span>
-            <span class='price'>Price</span>
-            <span class='price'>Pints</span>
-          </div>
-          <div *ngFor='let keg of kegMenu'>
-            <div [class]="colorCoding(keg)">
-              <span *ngIf="editMode"><p (click)="editInfo(keg)" [class]="checkboxBackground(keg)"></p></span>
-              <span class='type'>{{keg.name}}</span>
-              <span class='brand'>{{keg.brand}}</span>
-              <span class='alcohal'>{{keg.alcohalContent}} %</span>
-              <span class='price'>$ {{keg.price}} </span>
-              <span class='price'>{{keg.pints}} <p class="pints"></p>
-                <i *ngIf="type === 'Employee'" (click)="reducePintByone(keg)" class="glyphicon glyphicon-minus btn btn-danger btn-xs"></i>
+      <div *ngIf="pagination">
+        <div *ngIf="pagination != 'new'">
+          <h1>{{title}}</h1>
+          <div class="menu">
+            <div class='menuTitle'>
+              <span *ngIf="editMode">
+                Select
               </span>
+              <span class='species'>Species</span>
+              <span class='name'>Name</span>
+              <span class='age'> Age</span>
+              <span class='diet'>Diet</span>
+              <span class='location'>Location</span>
+              <span class='caretakers'> Caretakers</span>
+              <span class='sex'>Sex</span>
+              <span class='likes'>Likes</span>
+              <span class='dislikes'>Dislikes</span>
+            </div>
+            <div *ngFor='let animal of animals'>
+              <div *ngIf="ageRestriction(animal)">
+                <span *ngIf="editMode"><p (click)="editInfo(animal)" [class]="checkboxBackground()"></p></span>
+                <span class='species'>{{animal.species}}</span>
+                <span class='name'>{{animal.name}}</span>
+                <span class='age'>{{animal.age}}</span>
+                <span class='diet'>{{animal.diet}} </span>
+                <span class='location'>{{animal.location}} </span>
+                <span class='caretakers'>{{animal.caretakers}}</span>
+                <span class='sex'>{{animal.sex}}</span>
+                <span class='likes'>{{animal.likes}} </span>
+                <span class='dislikes'>{{animal.dislikes}} </span>
+              </div>
             </div>
           </div>
         </div>
-        <div *ngIf='selectedInfo'>
-          <h2> Edit Info </h2>
-          <div class="form-group">
-            <label>Enter Type:</label>
-            <input type="text" [(ngModel)]="selectedInfo.name" [value]="selectedInfo.name">
-          </div>
-          <div class="form-group">
-            <label>Enter Brand:</label>
-            <input type="text" [(ngModel)]="selectedInfo.brand" [value]="selectedInfo.Brand">
-          </div>
-          <div class="form-group">
-            <label>Enter Alcohal Percentage:</label>
-            <input type="number" [(ngModel)]="selectedInfo.alcohalContent" [value]="selectedInfo.Alcohal">
-          </div>
-          <div class="form-group">
-            <label>Enter Price in Dollars:</label>
-            <input type="number" [(ngModel)]="selectedInfo.price" [value]="selectedInfo.price">
-          </div>
-          <div class="form-group">
-            <label>Enter Available Pints:</label>
-            <input type="number" [(ngModel)]="selectedInfo.pints" [value]="selectedInfo.pints">
-          </div>
-          <input type="button" class="btn btn-success" value="Edit" (click)="finishedEditing()"/>
+        <div *ngIf="pagination == 'new'">
+          <form>
+            <div class="form-group">
+              <label>Enter Species</label>
+              <input type="text" class="species" placeholder="Species">
+            </div>
+            <div class="form-group">
+              <label>Enter Name</label>
+              <input type="text" class="name" placeholder="Name">
+            </div>
+            <div class="form-group">
+              <label>Enter Age</label>
+              <input type="number" class="age" placeholder="Age">
+            </div>
+            <div class="form-group">
+              <label>Enter Diet</label>
+              <input type="text" class="diet" placeholder="Diet">
+            </div>
+            <div class="form-group">
+              <label>Enter Location</label>
+              <input type="text" class="location" placeholder="Location">
+            </div>
+            <div class="form-group">
+              <label>Enter Caretakers</label>
+              <input type="number" class="caretakers" placeholder="Caretakers">
+            </div>
+            <div class="form-group">
+              <label>Enter Sex</label>
+              <input type="text" class="sex" placeholder="Sex">
+            </div>
+            <div class="form-group">
+              <label>Enter Likes</label>
+              <input type="text" class="likes" placeholder="Likes">
+            </div>
+            <div class="form-group">
+              <label>Enter Dislikes</label>
+              <input type="text" class="dislikes" placeholder="Dislikes">
+            </div>
+            <div class="form-group">
+              <input type="button" class="btn btn-success submitForm" value="Submit">
+            </div>
+          </form>
         </div>
-        <div *ngIf="type =='Employee'">
-          <input type="button" class="btn btn-success" value="Edit Menu" (click)="startEditing()" *ngIf="!editMode"/>
-          <input type="button" class="btn btn-success" value="Add New Item" (click)="addNewItem()" *ngIf="!editMode"/>
-
-        </div>
-        <button class="btn btn-info" (click)="goHome()">Home</button>
+        <hr>
+        <input type="button" *ngIf="pagination != 'new'" class="btn btn-success" value="Edit" (click)="edit()">
+        <input type="button" class="btn btn-success" value="Home" (click)="goToPage('home')">
       </div>
     </div>
   `
 })
 export class AppComponent{
-  type:string =null;
-  title: string = 'Kegs Menu';
-  kegMenu =KEGS;
-  selectedInfo:Kegs = null;
-  editMode: boolean =false;
-  editInfo(selectedKegs: Kegs){
-    this.selectedInfo = selectedKegs;
+  pagination :string = null;
+  editMode: boolean = false;
+  selectedAnimal: Animals = null;
+  goToPage(value: string){
+    if(value == 'home'){
+      this.pagination = null;
+    }else{
+      this.pagination = value;
+    }
   }
-  checkboxBackground(selectedKegs: Kegs){
-    if(this.selectedInfo == selectedKegs){
+  animals: Animals[] = ANIMALS;
+  ageRestriction(animal: Animals){
+    let returnValue = false;
+    if((this.pagination == "young" && animal.age < 2) ||(this.pagination == "mature" && animal.age > 1) || (this.pagination == "all")){
+      returnValue = true;
+    }
+    return returnValue;
+  }
+  edit(){
+    this.editMode = true;
+  }
+  checkboxBackground(animal:Animals){
+    if(this.selectedAnimal == animal){
       return 'checked';
     }else{
       return 'unChecked';
     }
   }
-  finishedEditing(){
-    this.selectedInfo = null;
-    this.editMode = false;
-  }
-  startEditing(){
-    this.editMode = true;
-  }
-  addNewItem(){
-
-  }
-  showMenu(name:string){
-    this.type = name;
-    console.log(this.type)
-  }
-  reducePintByone(keg: Kegs){
-    keg.pints -= 1;
-  }
-  colorCoding(keg: Kegs){
-    if(this.type == "Employee"){
-      if(keg.pints < 10){
-        return 'bg-danger';
-      }else if(keg.pints < 20){
-        return 'bg-warning';
-      }else{
-        return 'bg-info';
-      }
-    }else{
-      if(keg.price > 10){
-        return 'bg-danger';
-      }else if(keg.price > 5){
-        return 'bg-warning';
-      }else{
-        return 'bg-info';
-      }
-    }
-  }
-  goHome(){
-    this.type = null;
+  editInfo(animal:Animals){
+    this.selectedAnimal = animal;
   }
 }
-export class Kegs{
-  public pints: number = 124;
-  constructor(public name: string, public brand: string, public price: number, public alcohalContent:number){}
+export class Animals{
+  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: string, public sex: string, public likes: string, public dislikes: string){}
 }
-const KEGS: Kegs[] =[
-   new Kegs('Bear','Guinness', 5,4.1),
-   new Kegs('Bear','Orangeboom', 8,4.1),
-   new Kegs('Bear','Hoegaarden', 11,4.1),
-   new Kegs('Bear','Asahi Super Dry', 8,4.1),
-   new Kegs('Bear','Tsingtao', 3,4.1),
-   new Kegs('Bear','Estrella Damm Barcelona', 9,4.1),
-   new Kegs('Bear','Peroni Nastro Azzurro', 13,4.1)
+const ANIMALS: Animals[] =[
+   new Animals('Arctic Fox','Moon', 2,'Carnivore','Northern Trail',5,'Female','Cool shade','Loud noises'),
+   new Animals('Arctic Fox','Moon', 3,'Carnivore','Northern Trail',5,'Female','Cool shade','Loud noises'),
+   new Animals('Arctic Fox','Moon', 1,'Carnivore','Northern Trail',5,'Female','Cool shade','Loud noises')
  ]
